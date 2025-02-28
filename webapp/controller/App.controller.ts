bf6fca5ab79e8fe107229cbc7e55c3f4a7d2cd59
giselaa85 <gisela.agusti@gmail.com>
2025-02-28 23:24:08 +0000

    Navegacion

---
+++ webapp/controller/App.controller.ts
@@ -0,0 +1,13 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace logaligroup.logali.controller
+ */
+
+
+export default class App extends Controller {
+    public onInit(): void {
+
+    }
+
+}
