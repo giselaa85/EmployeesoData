c5e4be12e78421dba1ea3a8d5633acea759ecda3
giselaa85 <gisela.agusti@gmail.com>
2025-03-01 15:05:22 +0000

    Navigation to Order details

--- webapp/controller/OrderDetails.controller.ts
+++ webapp/controller/OrderDetails.controller.ts
@@ -1,13 +1,38 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import History from "sap/ui/core/routing/History";
+import UIComponent from "sap/ui/core/UIComponent";
+import { Button$PressEvent } from "sap/m/Button";
+import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 
 /**
  * @namespace logaligroup.logali.controller
  */
 
-
 export default class App extends Controller {
-    public onInit(): void {
-
+    public onInit(): void | undefined {
+        const oRouter = UIComponent.getRouterFor(this);
+        oRouter.getRoute("RouteOrderDetails")?.attachPatternMatched(this._onObjectMatched, this);
     }
+
+    public onBack(oEvent:Button$PressEvent): void | undefined {
+        const oHistory = History.getInstance();
+        const sPreviousHash = oHistory.getPreviousHash();
+        if(sPreviousHash !== undefined){
+            window.history.go(-1);            
+        }
+        else {
+            const oRouter = UIComponent.getRouterFor(this);
+            oRouter.navTo("RouteMain");
+        }
+    }
+
+    private _onObjectMatched(oEvent:Route$PatternMatchedEvent){
+        const args = oEvent.getParameter("arguments") as any;
+        this.getView()?.bindElement({
+            path: "/Orders(" + args.OrderID + ")",
+            model: "odataNorthwind"
+        })
+    }
+
 
 }
